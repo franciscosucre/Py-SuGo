ccc706eda7decbe9e5eb5bd969e40638149327cd
franciscosucre <franciscojsucreg@gmail.com>
2019-08-05 22:23:12 +0200

    middleware implemented

--- application.py
+++ application.py
@@ -1,7 +1,14 @@
-from urllib.parse import parse_qs
+# Standard libs imports
+import json
 from email.message import Message
+from http import HTTPStatus
+from io import BufferedReader
+from typing import Any, Callable, Dict, List, Optional, Tuple, cast, Union
+from urllib.parse import parse_qs
+from wsgiref.headers import Headers
+from wsgiref.util import is_hop_by_hop
 
-HTTP_HEADERS = [
+HTTP_HEADERS: List[str] = [
     "A_IM",
     "ACCEPTACCEPT_CHARSETACCEPT_DATETIMEACCEPT_ENCODING",
     "ACCEPT_LANGUAGE",
@@ -38,55 +45,78 @@ HTTP_HEADERS = [
     "WARNING",
 ]
 
+
 class Request():
+    headers: Headers = Headers()
+    body: Any = dict()
+
     def __init__(self, environ: dict):
-        self.wsgi_input = environ.get('wsgi.input')
+        self.wsgi_input: BufferedReader = cast(BufferedReader, environ.get('wsgi.input'))
         self.path = environ.get('PATH_INFO')
-        self.remote_address = environ.get('PATH_INFO')
         self.port = environ.get('PORT', '')
         self.query = parse_qs(environ.get('QUERY_STRING', ''))
-        self.content_length = environ.get('CONTENT_LENGTH', '')
-        self.headers = dict()
-        keys = list(map(lambda x: x.replace('HTTP_', ''),  environ.keys()))
-        for key in filter(lambda key: key in HTTP_HEADERS, keys):
-            self.headers[key.replace('HTTP_', '')] = environ.get(key)
+        for key in environ.keys():
+            replaced_key = key.replace('HTTP_', '')
+            if replaced_key in HTTP_HEADERS:
+                self.headers.add_header(replaced_key, environ.get(key))
         self._read_request_body()
 
-    def _read_request_body(self):
+    def _read_request_body(self: 'Request'):
         try:
-            request_body_size = int(self.headers.get('CONTENT_LENGTH', 0))
+            request_body_size = int(self.headers.get('CONTENT_LENGTH', '0'))
         except (ValueError):
             request_body_size = 0
         readbytes = self.wsgi_input.read(request_body_size)  # returns bytes object
         self.raw_body = readbytes.decode('utf-8')  # returns str object
 
 
-
-
-class Application:
-    def __init__(self, environ, start_response):
-        self._environ = environ
-        self._start_response = start_response
-        self.headers = dict()
-
-    def __iter__(self):
-        request = Request(self._environ)
-        
-        body = b'Hello world!\n'
-        status = '200 OK'
-        headers = [('Content-type', 'text/plain')]
-        query = parse_qs(self._environ.get('QUERY_STRING', ''))
-        self._start_response(status, headers)
-        yield body
-
-    def set_header(self, key, value):
-        self.headers[key] = value
-
-    def get_header(self, key):
-        return self.header.get(key)
-
-    
-
-
 class Response():
-    pass
+    headers: Headers = Headers()
+    request: Request
+    status_code: int = 200
+    body: Any = dict()
+
+    def __init__(self, start_response, request: Request):
+        self.request = request
+        self._start_response = start_response
+
+    def status(self: 'Response', status_code: int) -> 'Response':
+        self.status_code = status_code
+        return self
+
+    def json(self: 'Response', data: Dict):
+        self.headers.add_header('CONTENT_TYPE', 'application/json')
+        self.body = data
+        self._start_response('200 OK', self.headers.items())
+        string_body: str = json.dumps(data)
+        byte_body: bytes = string_body.encode('utf-8')
+        return byte_body
+
+
+NextFunction = Callable[[], Any]
+Middleware = Union[Callable[[Request, Response, NextFunction], Any], Callable[[Request, Response], Any]]
+
+
+class Application():
+    middlewares: List[Middleware] = list()
+    current_layer: int = 0
+
+    def __init__(self: 'Application', request_handler: Middleware):
+        self.request_handler = request_handler
+
+    def __call__(self: 'Application', environ, start_response):
+        self.current_layer: int = 0
+        request = Request(environ)
+        response = Response(start_response, request)
+
+        def next_handler():
+            if self.current_layer >= len(self.middlewares):
+                return self.request_handler(request, response)
+            layer = self.middlewares[self.current_layer]
+            self.current_layer += 1
+            return layer(request, response, next_handler)
+
+        yield next_handler()
+
+    def use_middleware(self: 'Application', middleware: Middleware):
+        self.middlewares.append(middleware)
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from application import Application
-
+from application import Application, Request, Response, NextFunction
+import json
 from wsgiref.simple_server import make_server, WSGIServer
+
 
 class Server(WSGIServer):
     pass
+
     def finish_request(self, request, client_address):
         return super().finish_request(request, client_address)
 
 
+def handle(request: Request, response: Response, next=None):
+    response.status(201)
+    return response.json({"hola": "mundo"})
 
+
+def parse_body_json(request: Request, response: Response, next_handler: NextFunction):
+    try:
+        request.body = json.loads(request.raw_body)
+    except:
+        request.body = dict()
+    return next_handler()
+
+def log_response(request: Request, response: Response, next_handler: NextFunction):
+    result = next_handler()
+    print(response.body)
+    return result
 
 
 if __name__ == "__main__":
-    server = make_server('localhost', 5000, Application)
+    app = Application(handle)
+    app.use_middleware(log_response)
+    app.use_middleware(parse_body_json)
+    server = make_server('localhost', 5000, app)
     server.serve_forever()
