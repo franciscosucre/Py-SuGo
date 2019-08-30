ef6422d9166c666759182e705185ebf5a935500e
fsucre <fsucre@aluxion.com>
2019-08-30 16:12:16 +0200

    refactor response methods

--- py_sugo/response.py
+++ py_sugo/response.py
@@ -5,7 +5,7 @@ from typing import Dict
 from wsgiref.headers import Headers
 
 # First party libs imports
-from py_sugo.core import CONTENT_TYPE, CONTENT_LENGTH
+from py_sugo.core import CONTENT_TYPE, CONTENT_LENGTH, UTF_8
 from py_sugo.request import Request
 
 
@@ -28,13 +28,18 @@ class Response:
         return self
 
     def json(self: 'Response', data: Dict):
-        http_status = self._get_http_status(self.status_code)
-        string_body: str = json.dumps(data)
-        self.body = bytes(string_body, 'utf-8')
         self.headers.add_header(CONTENT_TYPE, 'application/json')
+        return self.send(json.dumps(data).encode(UTF_8))
+
+    def send(self: 'Response', body: bytes):
+        self.body = body
         self.headers.add_header(CONTENT_LENGTH, str(len(self.body)))
-        self._start_response('%d %s' % (http_status.value, http_status.phrase), self.headers.items())
+        self._start_response(self._get_wsgi_http_status(self.status_code), self.headers.items())
         return self.body
 
-    def _get_http_status(self: 'Response', status_code: int):
-        return list(filter(lambda h: h.value == status_code, list(HTTPStatus))).pop()
+    @staticmethod
+    def _get_wsgi_http_status(status_code: int) -> str:
+        for http_status in HTTPStatus:
+            if http_status.value == status_code:
+                return '%d %s' % (http_status.value, http_status.phrase)
+        raise ValueError("Unsupported HTTP Code: '%d'" % status_code)
